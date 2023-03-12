b6c6f57ca05fd939ab60a6a948969b4c61455e62
ranganathnk <ranganathnkulkarni@gmail.com>
2023-03-12 16:44:50 +0530

    sending response

--- 3/node-server/app.js
+++ 3/node-server/app.js
@@ -1,8 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.headers, req.method);
-//   process.exit()  //Exits from the server execution
+  //  console.log(req.url, req.headers, req.method);
+  //   process.exit()  //Exits from the server execution
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head></head>");
+  res.write("<body>From Node JS server..!</body>");
+  res.write("</html>");
+  res.end();
+  // res.write();  //We can't. Because it ended at the above line
 });
 
 server.listen(3000);
