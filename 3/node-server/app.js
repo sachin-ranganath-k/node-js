451b085c45a5cfd8505ce8ce8db8873eec39b55b
ranganathnk <ranganathnkulkarni@gmail.com>
2023-03-12 18:12:30 +0530

    write static data into file and redirection

--- 3/node-server/app.js
+++ 3/node-server/app.js
@@ -1,20 +1,36 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   //  console.log(req.url, req.headers, req.method);
   //   process.exit()  //Exits from the server execution
+  const url = req.url;
+  const method = req.method;
 
-/*
+  /*
     Matches the URL with /. And output will be form.
     Once click on Send button, output will be From Node JS server..!. Because matches the line which are after if()
 */
-if(req.url==="/"){
+  if (url === "/") {
     res.write("<html>");
     res.write("<head></head>");
-    res.write("<body><form action=/message><input type=text><button type=submit>Send</button></form></body>");
+    res.write(
+      "<body><form action=/message method=POST><input type=text><button type=submit>Send</button></form></body>"
+    );
     res.write("</html>");
     return res.end(); //return: will execute the next lines of code. if not return, will not execute other lines of code
-}
+  }
+
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "Dummy Text");
+    res.statusCode = 302; 
+    //302 : Indicates that the resource requested has been temporarily moved to the URL given by the Location header.
+    //Can be seen in Network tab
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head></head>");
