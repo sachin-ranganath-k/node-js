d20590b1ebccc4c07f12fe2c103ccd02998c1ada
ranganathnk <ranganathnkulkarni@gmail.com>
2023-03-12 17:34:55 +0530

    request url

--- 3/node-server/app.js
+++ 3/node-server/app.js
@@ -4,6 +4,17 @@ const server = http.createServer((req, res) => {
   //  console.log(req.url, req.headers, req.method);
   //   process.exit()  //Exits from the server execution
 
+/*
+    Matches the URL with /. And output will be form.
+    Once click on Send button, output will be From Node JS server..!. Because matches the line which are after if()
+*/
+if(req.url==="/"){
+    res.write("<html>");
+    res.write("<head></head>");
+    res.write("<body><form action=/message><input type=text><button type=submit>Send</button></form></body>");
+    res.write("</html>");
+    return res.end(); //return: will execute the next lines of code. if not return, will not execute other lines of code
+}
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head></head>");
