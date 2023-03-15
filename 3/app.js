c4724abb8e99b86981807dc7eaac5b863669b41b
ranganathnk <ranganathnkulkarni@gmail.com>
2023-03-15 16:40:23 +0530

    body-parser

--- 3/app.js
+++ 3/app.js
@@ -1,17 +1,29 @@
 const http = require("http");
 
 const express = require("express");
+const bodyParser = require("body-parser");
+//body-parser parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-   res.send("Always runs")
-    next();  //WIll be unresponsive if not added next() here
-    //Only this will be shown for all routes as it has /
+app.use(bodyParser.urlencoded({ extended: true }));
+/*
+bodyParser.urlencoded() :--> A function provided by the body-parser package that 
+returns a middleware function that can parse incoming request bodies containing URL-encoded data. 
+
+extended:false --> The extended option allows to choose between parsing the URL-encoded data with the querystring library (when false) 
+extended:true --> The qs (query string) library (when true).
+*/
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="productTitle"><button type="submit">Add Product</button></form>'
+  );
 });
 
-app.use("/cart", (req, res, next) => {
-  res.send("<h2>Path /cart route</h2>");
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/"); //Redirects to /
 });
 
 app.use("/", (req, res, next) => {
