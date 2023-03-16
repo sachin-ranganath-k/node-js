5c44461555f550762ba6c5efd5c7b5556808325a
ranganathnk <ranganathnkulkarni@gmail.com>
2023-03-16 15:19:51 +0530

    filtering path (/add-product -> /admin/add-product) comments added

--- 3/routes/admin.js
+++ 3/routes/admin.js
@@ -5,12 +5,16 @@ const router = express.Router();
 //app.use executes for all methods like GET, POST.
 //To overcome we can use app.get, app.post.....
 
+
+// /admin/add-product : GET
 router.get("/add-product", (req, res, next) => {
   res.send(
     '<form action="/admin/product" method="POST"><input type="text" name="productTitle"><button type="submit">Add Product</button></form>'
   );
 });
 
+
+// /admin/product : POST
 router.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/"); //Redirects to /
