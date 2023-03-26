5f0936d95969517296be17bcb0bd1df6f0d6d864
ranganathnk <ranganathnkulkarni@gmail.com>
2023-03-26 18:12:03 +0530

    handle CORS error

--- node-rest-api/app.js
+++ node-rest-api/app.js
@@ -5,10 +5,15 @@ const app = express();
 const feedRoutes = require("./routes/feed");
 
 //app.use(bodyParser.urlEncoded())  //x-www-form-urlencoded, if <form>
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 app.use(bodyParser.json()); // application/json. Parses to json
+
+//Handling CORS error
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use("/feed", feedRoutes); //  GET /feed/posts, POST /feed/post
 
